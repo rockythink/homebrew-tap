0b999f2f271b46c9d1ded6dd247ffd2c758b007f
Elazer <31272334+rockythink@users.noreply.github.com>
2026-09-05 03:45:23 +0800

    danmu v0.4.1

--- Formula/danmu.rb
+++ Formula/danmu.rb
@@ -1,26 +1,26 @@
 class Danmu < Formula
   desc "Live interaction console for knowledge streamers"
   homepage "https://github.com/rockythink/shisui-danmu"
-  version "0.4.0"
+  version "0.4.1"
   license "MPL-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.0/shisui-danmu-macos-aarch64.tar.gz"
-      sha256 "2df8b8749f65d923ebc8604cd07654f737c89adf6929e47c1b82a3094befde93"
+      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.1/shisui-danmu-macos-aarch64.tar.gz"
+      sha256 "a24bf28780174faa8cc09c4b76936b698c7ad38224e4aaa7ee6c109d06163247"
     else
-      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.0/shisui-danmu-macos-x86_64.tar.gz"
-      sha256 "808467d64f056fe732870d1265b255fb244445094a99d348c0dc9ec2d54aca54"
+      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.1/shisui-danmu-macos-x86_64.tar.gz"
+      sha256 "19e09ea05e5c350117f036da89a32d79c8d5f5d17452e9b948d6196d91672cb2"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.0/shisui-danmu-linux-aarch64.tar.gz"
-      sha256 "f1afd41eefb9d6d5b2ec25d49ebba543dcfca00c3d204644a829827e913cedb8"
+      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.1/shisui-danmu-linux-aarch64.tar.gz"
+      sha256 "9da73f7ded5a1e212a9336ba78226dec42c66149c7c3c23425c2ac5fbc2a850a"
     else
-      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.0/shisui-danmu-linux-x86_64.tar.gz"
-      sha256 "a5d13ddda23d86e97d26997f26088b59a7f402ac0e84b78c74f86a0cb01c89e4"
+      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.1/shisui-danmu-linux-x86_64.tar.gz"
+      sha256 "7a80d083bd59f258ef48359be995c8117377b0d8787051fba9bcec9a99ed23fe"
     end
   end
 
