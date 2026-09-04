f73e291ba48b000c2196a3cc6106c6a89fd7c6b8
Elazer <31272334+rockythink@users.noreply.github.com>
2026-09-05 07:08:51 +0800

    danmu v0.4.2

--- Formula/danmu.rb
+++ Formula/danmu.rb
@@ -1,26 +1,26 @@
 class Danmu < Formula
   desc "Live interaction console for knowledge streamers"
   homepage "https://github.com/rockythink/shisui-danmu"
-  version "0.4.1"
+  version "0.4.2"
   license "MPL-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.1/shisui-danmu-macos-aarch64.tar.gz"
-      sha256 "a24bf28780174faa8cc09c4b76936b698c7ad38224e4aaa7ee6c109d06163247"
+      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.2/shisui-danmu-macos-aarch64.tar.gz"
+      sha256 "5b4baa89830c1204aec03c977acd10cb5210f44c5e8d76f57cfe2e24e95cb014"
     else
-      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.1/shisui-danmu-macos-x86_64.tar.gz"
-      sha256 "19e09ea05e5c350117f036da89a32d79c8d5f5d17452e9b948d6196d91672cb2"
+      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.2/shisui-danmu-macos-x86_64.tar.gz"
+      sha256 "6725bb767e81e4fb518aec45db726564fe3ada09a3d86642893e530fb4a97595"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.1/shisui-danmu-linux-aarch64.tar.gz"
-      sha256 "9da73f7ded5a1e212a9336ba78226dec42c66149c7c3c23425c2ac5fbc2a850a"
+      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.2/shisui-danmu-linux-aarch64.tar.gz"
+      sha256 "1e1154610cce8779492942b2a74452cf836e92224cbb54d64a81e8a065c24aab"
     else
-      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.1/shisui-danmu-linux-x86_64.tar.gz"
-      sha256 "7a80d083bd59f258ef48359be995c8117377b0d8787051fba9bcec9a99ed23fe"
+      url "https://github.com/rockythink/shisui-danmu/releases/download/v0.4.2/shisui-danmu-linux-x86_64.tar.gz"
+      sha256 "4b29f83a016342bfcc81c28f912e89211d422c14a24cc372ea7efc5d31ca2180"
     end
   end
 
